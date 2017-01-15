72dc71c4888aebd67d9bdcff2b19f69e79356684
Mark Huo <huomian@outlook.com>
2017-01-15 21:03:33 +0800

    chapter2 easy

--- homework/chapter2/src/Main.purs
+++ homework/chapter2/src/Main.purs
@@ -2,8 +2,14 @@ module Main where
 
 import Prelude
 import Control.Monad.Eff (Eff)
-import Control.Monad.Eff.Console (CONSOLE, log)
+import Control.Monad.Eff.Console (CONSOLE, logShow)
+import Math (pi, sqrt)
+
+diagonal :: Number -> Number -> Number
+diagonal w h = sqrt (w * w + h * h)
+
+circleArea :: Number -> Number
+circleArea r = 2.0 * pi * r
 
 main :: forall e. Eff (console :: CONSOLE | e) Unit
-main = do
-  log "Hello sailor!"
+main = logShow (diagonal 3.0 4.0)
